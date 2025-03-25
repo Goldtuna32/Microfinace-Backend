11244514fb594590965f4ffd985489ad1b10e56a
kaungkhantzaw19 <kaungkhantzawsonny@gmail.com>
2025-03-25 14:19:34 +0630

    Latest Config

--- src/main/java/com/sme/config/SecurityConfig.java
+++ src/main/java/com/sme/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login","/api/auth/refresh").permitAll()
-                        .requestMatchers("/api/branches").hasRole("ADMIN")
+                        .requestMatchers("/api/branches/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(customUserDetailsService)

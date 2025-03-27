ea7931b1a67ffa117b124070077f97190a0d7160
ZyH12356d <107683128+ZyH12356d@users.noreply.github.com>
2025-03-27 10:27:13 +0630

    Final Pull

--- src/main/java/com/sme/config/SecurityConfig.java
+++ src/main/java/com/sme/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login","/api/auth/refresh").permitAll()
                         .requestMatchers("/api/branches/**").hasRole("ADMIN")
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(customUserDetailsService)
